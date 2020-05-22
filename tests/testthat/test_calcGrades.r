d403ea6145b6fcd1566ebbb1a28b5b20250d1ce4
Taylor <tbrown122387@gmail.com>
2020-05-22 14:36:32 -0400

    add a test for the new function

--- tests/testthat/test_calcGrades.r
+++ tests/testthat/test_calcGrades.r
@@ -1,10 +1,14 @@
 context("Tests the grading function")
 
-test_that("test missing args", {
+test_that("test missing args for calcGrades", {
 
   # check that it throws error when you don't supply all arguments
   expect_error(calcGrades())
 
+})
+
+
+test_that("test correct output for calcGrades", {
 
   submissions <- "example/assignment1_submissions/"
   my_test_file <- "example/grade_hw1.R"
@@ -15,5 +19,15 @@ test_that("test missing args", {
   expect_equal(results[2,2], 0)
   expect_equal(results[1,3], 1)
   expect_equal(results[2,3], 1)
+
+})
+
+test_that("test bad args for gradescope function", {
+
+  # check that it throws error when you don't supply all arguments
+  expect_error(calcGradesForGradescope("fake_file.R", "anotherfakeFile.R", "bad_arg"))
+
+  # missing args doesn't fly
+  expect_error(calcGradesForGradescope())
 
 })
