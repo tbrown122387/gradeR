5ff8f5e0cc6ddf2f82bd337708f7b315c7131dbc
Pete Benbow <pebenbow@davidson.edu>
2026-01-16 15:20:14 -0500

    Updates to documentation and vignettes

--- vignettes/example/grading_resources/assignment1_grading_file.r
+++ vignettes/example/grading_resources/assignment1_grading_file.r
@@ -3,22 +3,22 @@ library(testthat)
 # first test
 test_that("first", {
   
-  expect_equal( sum(myVector), 6) #22.17444, tolerance=1e-4)
+  expect_equal( sum(myVector), 6, label = "myVector sums to 6" ) #22.17444, tolerance=1e-4)
 
   })
 
 # second test
 test_that("second", {
 
-  expect_true(is.character(myString))
-  expect_true(len(myString) > 2)
+  expect_true(is.character(myString), label = "myString is character data type")
+  expect_true(len(myString) > 2, label = "myString has length greater than 2 characters")
 
 })
 
 # third test
 test_that("third", {
   
-
   expect_equal(myDataFrame[1,1], 700.01, tolerance=1e-3)
-  expect_equal(nrow(myDataFrame), 2)
+  expect_equal(nrow(myDataFrame), 2, label = "myDataFrame has 2 rows/observations")
+  
 })
